08f52e1ad14f1dc8afcd71874aa8333f7b6805a5
Lim Swee Kiat <limsweekiat@gmail.com>
2019-06-03 22:40:31 +0800

    Transition demo

---
+++ data.js
@@ -0,0 +1,37 @@
+data = [
+	{
+		"color": "#1abc9c",
+		"race": "Malay",
+		"race_pos": 1,
+		"hue": "Cool",
+		"hue_pos": 1
+	},
+	{
+		"color": "#f1c40f",
+		"race": "Malay",
+		"race_pos": 2,
+		"hue": "Warm",
+		"hue_pos": 1
+	},
+	{
+		"color": "#e74c3c",
+		"race": "Indian",
+		"race_pos": 1,
+		"hue": "Warm",
+		"hue_pos": 2
+	},
+	{
+		"color": "#3498db",
+		"race": "Chinese",
+		"race_pos": 1,
+		"hue": "Cool",
+		"hue_pos": 2
+	},
+	{
+		"color": "#34495e",
+		"race": "Chinese",
+		"race_pos": 2,
+		"hue": "Cool",
+		"hue_pos": 3
+	},
+]
